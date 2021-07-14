7b166069f22aba73116bab828317f9051184d06d
Danny Roubin <dannyroubin1@gmail.com>
2021-07-14 13:46:36 -0700

    created function to add new notes to the existing list

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,52 +1,64 @@
-// // LOAD DATA
-// // We are linking our routes to a series of "data" sources.
-// // These data sources hold arrays of information on table-data, waitinglist, etc.
+const fs = require("fs");
+let dataStop1 = fs.readFileSync("./db/db.json", "utf-8");
+let dataStop2 = JSON.parse(dataStop1);
 
-// const tableData = require('../data/tableData');
-// const waitListData = require('../data/waitinglistData');
+function addToNoteList() {
+  fs.writeFile("./db/db.json", JSON, stringify(dataStop2), (err) => {
+    if (err) console.log(err);
+  });
+}
 
-// // ROUTING
+// Ignore everything under this marker, using the below as a reference
 
-// module.exports = (app) => {
-//   // API GET Requests
-//   // Below code handles when users "visit" a page.
-//   // In each of the below cases when a user visits a link
-//   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-//   // ---------------------------------------------------------------------------
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
 
-//   app.get('/api/tables', (req, res) => res.json(tableData));
+const tableData = require("../data/tableData");
+const waitListData = require("../data/waitinglistData");
 
-//   app.get('/api/waitlist', (req, res) => res.json(waitListData));
+// ROUTING
 
-//   // API POST Requests
-//   // Below code handles when a user submits a form and thus submits data to the server.
-//   // In each of the below cases, when a user submits form data (a JSON object)
-//   // ...the JSON is pushed to the appropriate JavaScript array
-//   // (ex. User fills out a reservation request... this data is then sent to the server...
-//   // Then the server saves the data to the tableData array)
-//   // ---------------------------------------------------------------------------
+module.exports = (app) => {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
 
-//   app.post('/api/tables', (req, res) => {
-//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-//     // It will do this by sending out the value "true" have a table
-//     // req.body is available since we're using the body parsing middleware
-//     if (tableData.length < 5) {
-//       tableData.push(req.body);
-//       res.json(true);
-//     } else {
-//       waitListData.push(req.body);
-//       res.json(false);
-//     }
-//   });
+  app.get("/api/tables", (req, res) => res.json(tableData));
 
-//   // I added this below code so you could clear out the table while working with the functionality.
-//   // Don"t worry about it!
+  app.get("/api/waitlist", (req, res) => res.json(waitListData));
 
-//   app.post('/api/clear', (req, res) => {
-//     // Empty out the arrays of data
-//     tableData.length = 0;
-//     waitListData.length = 0;
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
 
-//     res.json({ ok: true });
-//   });
-// };
+  app.post("/api/tables", (req, res) => {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    } else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", (req, res) => {
+    // Empty out the arrays of data
+    tableData.length = 0;
+    waitListData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,25 +1,27 @@
-// // DEPENDENCIES
-// // We need to include the path package to get the correct file path for our html
+// Ignore everything under this marker, using the below as a reference
 
-// const path = require('path');
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
 
-// // ROUTING
+const path = require("path");
 
-// module.exports = (app) => {
-//   // => HTML GET Requests
-//   // Below code handles when users "visit" a page.
-//   // In each of the below cases the user is shown an HTML page of content
+// ROUTING
 
-//   app.get('/tables', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/tables.html'));
-//   });
+module.exports = (app) => {
+  // => HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
 
-//   app.get('/reserve', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/reserve.html'));
-//   });
+  app.get("/tables", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/tables.html"));
+  });
 
-//   // If no matching route is found default to home
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/home.html'));
-//   });
-// };
+  app.get("/reserve", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
